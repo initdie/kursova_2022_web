d10d969403d3bf514448a9a3a00f0c81f834f3e6
Dima <initdie@gmail.com>
2022-11-15 10:55:07 +0200

    commit

---
+++ client/src/components/authorization/Login.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './authorization.css'
+import Input from "../../utils/input/Input";
+import {useDispatch} from "react-redux";
+import {login} from "../../actions/user";
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+
+    return (
+        <div className='authorization'>
+            <div className="authorization__header">Авторизація</div>
+            <Input value={email} setValue={setEmail} type="text" placeholder="Введіть email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введіть пароль..."/>
+            <button className="authorization__btn" onClick={() => dispatch(login(email, password))}>Увійти</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/components/authorization/Registration.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import './authorization.css'
+import Input from "../../utils/input/Input";
+import {registration} from "../../actions/user";
+
+const Registration = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <div className='authorization'>
+            <div className="authorization__header">Реєстрація</div>
+            <Input value={email} setValue={setEmail} type="text" placeholder="Введіть email..."/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Введіть пароль..."/>
+            <button className="authorization__btn" onClick={() => registration(email, password)}>Зареєструватись</button>
+        </div>
+    );
+};
+
+export default Registration;
